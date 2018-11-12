46f158f2791c8fdf57d044de4fc1b8e540217d69
Brian Costlow <costlow.brian@gmail.com>
2018-11-12 15:17:23 -0800

    added node app skeleton structure

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700' rel='stylesheet' type='text/css'>
+  <title>Tradewater Plumbing</title>
+  <meta name="keywords" content="plumbing services, eugene plumbing, residential plumbing, commercial plumbing">
+  <meta name="description" content="Providing reliable plumbing to the greater Eugene-Springfield area">
+
+  <!-- build:css assets/styles/styles.css -->
+  <link rel="stylesheet" href="temp/styles/styles.css">
+  <!-- endbuild -->
+  <!-- build:js assets/scripts/Vendor.js -->
+  <script src="/temp/scripts/Vendor.js"></script>
+  <!-- endbuild -->
+</head>
+<body>
+  <header class="site-header">
+    <h1 class="site-title">Tradewater Plumbing</h1>
+    
+  </header>
+</body>

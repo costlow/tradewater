9d73f491a5b0d9f6b7ca6ef7b34f4c223bcc6f2c
Brian Costlow <costlow.brian@gmail.com>
2018-11-24 17:22:12 -0800

    Styles for header region

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -6,8 +6,8 @@ autoprefixer = require('autoprefixer');
 gulp.task('styles', function() {
   console.log('styles');
 
-  return gulp.src('./app/assets/styles/styles.css')
-    .pipe(sass({fiber: Fiber})
+  return gulp.src('./app/assets/styles/styles.scss')
+    .pipe(sass({fiber: Fiber, includePaths: "./node_modules/normalize-scss/sass"})
     .on('error', sass.logError))
     .pipe(gulp.dest('./app/temp/styles/'));
 });
